9f7285ee35622dfba76ff2c7c7ae0b3863e48949
wanwork <wjdqo9705@gmail.com>
2023-07-17 23:34:57 +0900

    Create week03_assignment.js
    알고리즘 공부중

---
+++ week03_assignment.js
@@ -0,0 +1,35 @@
+//1
+
+// const arr = [100, 200, 300];
+
+// arr.splice(1, 2, 150, 250, 500);
+
+// console.log(arr);
+
+//2
+
+// const arr = [1, 3, 4, 6, 9];
+// const reverseArr = [];
+
+// for(let i = arr.length - 1 ; i >=0 ; i--) {
+  
+//   reverseArr.push(arr[i]);
+// }
+
+// console.log(reverseArr);
+
+//3
+
+// 구구단
+
+for(let i = 2 ; i < 10 ; i++) {
+  let num = i;
+  let num2 = i -1;
+  let result = num * i;
+  
+  console.log(`${num}단
+ ${num}*${i}=${result}`)
+  
+  
+}
+
